5b0bc203b947d1d6ec3bd0a87afc32520d3691f2
nihal86 <nihalkbarnwal20@gmail.com>
2024-09-30 18:41:53 +0530

    routing setup using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Home';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home'; // Ensure the path is correct
+import Login from './Components/Login'; // Ensure the path is correct
+import Nav from './Components/Nav'; // If needed, can be included here
+import Footer from './Components/Footer'; // If needed, can be included here
 
 function App() {
-  return (
-    <div>
-      <Home></Home>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                {/* Add more routes for other components/pages as needed */}
+            </Routes>
+            <Footer /> {/* Footer component (if it's part of every page) */}
+        </Router>
+    );
 }
 
 export default App;
